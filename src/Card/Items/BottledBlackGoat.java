fcea564393237d85ec8b87b01cf1de10503bd7b6
tiago-rods <t.alves.rodrigues@hotmail.com>
2025-10-30 14:50:56 -0300

    Incluido lógica Player, package Items e suas classes.

---
+++ src/Card/Items/BottledBlackGoat.java
@@ -0,0 +1,15 @@
+package Items;
+
+public class BottledBlackGoat implements Items {
+    @Override
+    public String name() {return "Bottled Black Goat";}
+
+    @Override
+    public String description() {return "When used, the player receives a free black goat";}
+
+    @Override
+    public Boolean canUse(); // FAZER QUANDO LÓGICA ESTIVER CORRETA
+
+    @Override
+    public void use(); // FAZER QUANDO LÓGICA ESTIVER CORRETA
+}
---
+++ src/Card/Items/MagpiesLens.java
@@ -0,0 +1,10 @@
+package Items;
+
+public class MagpiesLens implements Items{
+    @Override
+    public String name() {return "Magpies Lens";}
+
+    @Override
+    public String description() {return "Search in the Creatures deck for one card of interest";}
+
+}
